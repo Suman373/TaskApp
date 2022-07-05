dfea5fdf34e5f5afb0521a65abee45d6d80b126f
Mohamed Hedi AYADI <mohamed.hedi.ayadi@outlook.com>
2022-07-05 14:52:52 +0100

    Added width: 15%; to the add task button to align it with the tasks

--- src/components/Addtask.jsx
+++ src/components/Addtask.jsx
@@ -1,60 +1,63 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Addtask extends Component {
     // the state contains the added task, app level state
     state = {
-        title:''
-    }
-      // title --> save
-      inputSave =(e)=>{
+        title: "",
+    };
+    // title --> save
+    inputSave = (e) => {
         e.preventDefault();
         this.props.addTask(this.state.title);
         this.setState({
-            title: ''
+            title: "",
         });
-    }
+    };
 
     // text input -> title -> title in setState
-    inputReceive =(e)=> this.setState({
-        title: e.target.value
-    });
-  
+    inputReceive = (e) =>
+        this.setState({
+            title: e.target.value,
+        });
+
     render() {
         return (
-            <header className='header'>
+            <header className="header">
                 <form onSubmit={this.inputSave}>
-                    <input type="text" 
-                    name="task" 
-                    style={addInputStyle} 
-                    placeholder='Enter your task'
-                    value={this.state.title}
-                    onChange={this.inputReceive}
+                    <input
+                        type="text"
+                        name="task"
+                        style={addInputStyle}
+                        placeholder="Enter your task"
+                        value={this.state.title}
+                        onChange={this.inputReceive}
                     />
-                    <button type='submit' id="addBtn" style={addbtnStyle}>Add</button>
+                    <button type="submit" id="addBtn" style={addbtnStyle}>
+                        Add
+                    </button>
                 </form>
             </header>
-        )
+        );
     }
 }
 const addInputStyle = {
-    fontSize: '1.2rem',
-    height: '2.5rem',
-    margin: '1rem auto',
-    padding: '5px',
-    width: '85%',
-    border: '2px solid orangered',
-    outline: 'none',
-    fontFamily: 'Marcellus SC,serif'
-
-}
+    fontSize: "1.2rem",
+    height: "2.5rem",
+    margin: "1rem auto",
+    padding: "5px",
+    width: "85%",
+    border: "2px solid orangered",
+    outline: "none",
+    fontFamily: "Marcellus SC,serif",
+};
 const addbtnStyle = {
-    fontSize: '1.2rem',
-    padding: '7px',
-    color: 'orangered',
-    cursor: 'pointer',
-    background: '#f7f2aa',
-    border: '2px solid orangered'
-}
+    fontSize: "1.2rem",
+    padding: "7px",
+    color: "orangered",
+    cursor: "pointer",
+    background: "#f7f2aa",
+    border: "2px solid orangered",
+    width: "15%",
+};
 
 export default Addtask;
